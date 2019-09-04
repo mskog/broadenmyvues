4b98adaa97915f1e94d057f8393eea5b43ee8d68
Magnus Skog <magnus.m.skog@gmail.com>
2019-09-04 20:30:58 +0200

    Better search for tv shows as well

--- src/components/search/tv_shows/Result.vue
+++ src/components/search/tv_shows/Result.vue
@@ -1,26 +1,24 @@
 <template>
-  <div class="tv_show">
-    <div class="columns is-mobile">
-      <div class="column is-one-third">
-        <router-link :to="`/search/tv_shows/details/${imdb_id}`">
-          <lazy-component
-            @show="loadPoster({ type: 'tv_show', tmdb_id: tmdb_id })"
-          >
-            <img :src="getPoster('tv_show', tmdb_id)" class="poster" />
-          </lazy-component>
-        </router-link>
-      </div>
-      <div class="column">
-        <router-link :to="`/search/tv_shows/details/${imdb_id}`">
-          <h2 class="title">{{ title }}</h2>
-        </router-link>
-        <div class="is-size-7">
-          <div class="level is-mobile">
-            <div class="level-left">
-              <div class="level-item">
-                <b-icon pack="fa" icon="calendar" size="is-small"></b-icon>
-                <span>{{ year }}</span>
-              </div>
+  <div class="tv_show_content">
+    <div class="column is-3-desktop is-4-table is-4-mobile">
+      <router-link :to="`/search/tv_shows/details/${imdb_id}`">
+        <lazy-component
+          @show="loadPoster({ type: 'tv_show', tmdb_id: tmdb_id })"
+        >
+          <img :src="getPoster('tv_show', tmdb_id)" class="poster" />
+        </lazy-component>
+      </router-link>
+    </div>
+    <div class="column title">
+      <router-link :to="`/search/tv_shows/details/${imdb_id}`">
+        <h2 class="title is-size-5">{{ title }}</h2>
+      </router-link>
+      <div class="is-size-7">
+        <div class="level is-mobile">
+          <div class="level-left">
+            <div class="level-item">
+              <b-icon pack="fa" icon="calendar" size="is-small"></b-icon>
+              <span>{{ year }}</span>
             </div>
           </div>
         </div>
@@ -59,7 +57,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-div.tv_show {
+div.tv_show_content {
   background: #1e2748;
   border-radius: 3px;
   position: relative;
@@ -79,10 +77,11 @@ div.tv_show {
 }
 
 img.poster {
-  top: -1.5em;
-  height: 9em;
-  width: 6em;
+  bottom: 2rem;
+  height: 100%;
+  max-width: 20%;
   position: absolute;
-  border-radius: 3px;
+  border-radius: 7px;
+  box-shadow: 0px 2px 3px rgba(0, 0, 0, 0.75);
 }
 </style>
--- src/components/search/tv_shows/Results.vue
+++ src/components/search/tv_shows/Results.vue
@@ -1,12 +1,15 @@
 <template>
-  <ul>
-    <li
-      v-for="result in results"
-      v-bind:key="`${result.title}-${result.imdb_id}`"
-    >
-      <Result v-bind="result" />
-    </li>
-  </ul>
+  <div class="tv_shows">
+    <div class="columns is-multiline">
+      <div
+        v-for="result in results"
+        v-bind:key="`${result.title}-${result.imdb_id}`"
+        class="tv_show column is-half"
+      >
+        <Result v-bind="result" class="columns is-mobile" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -19,12 +22,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-ul {
-  padding-top: 5em;
+.tv_shows  {
+  margin-top: 3rem;
 }
 
-li {
-  margin-bottom: 4em;
+.tv_show {
+  padding-top: 4rem;
+  padding-right: 2rem;
 }
 </style>
-
