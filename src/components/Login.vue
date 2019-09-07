54cbb9208bb92bcb6b50907e4582f307d0ee1f89
Magnus Skog <magnus.m.skog@gmail.com>
2019-09-07 17:09:37 +0200

    Fixed login on desktop

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,21 +1,25 @@
 <template>
-  <section>
-    <div class="box">
-      <b-field
-        label="Username"
-        :type="{ 'is-danger': !valid }"
-        :message="{ 'Invalid username': !valid }"
-      >
-        <b-input v-model="username"></b-input>
-      </b-field>
-      <b-field
-        label="Password"
-        :type="{ 'is-danger': !valid }"
-        :message="{ 'Invalid password': !valid }"
-      >
-        <b-input type="password" v-model="password"></b-input>
-      </b-field>
-      <button @click="submit" class="button is-success">Submit</button>
+  <section class="section">
+    <div class="container">
+      <div class="columns is-centered">
+        <div class="column is-6">
+          <b-field
+            label="Username"
+            :type="{ 'is-danger': !valid }"
+            :message="{ 'Invalid username': !valid }"
+          >
+            <b-input v-model="username"></b-input>
+          </b-field>
+          <b-field
+            label="Password"
+            :type="{ 'is-danger': !valid }"
+            :message="{ 'Invalid password': !valid }"
+          >
+            <b-input type="password" v-model="password"></b-input>
+          </b-field>
+          <button @click="submit" class="button is-success">Submit</button>
+        </div>
+      </div>
     </div>
   </section>
 </template>
